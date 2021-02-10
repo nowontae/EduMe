85ceaad919fe20dbb76698d18a84bb0263c6a652
seona <seona@DESKTOP-TFDSTUN>
2021-02-10 19:27:27 +0900

    선아_강좌만들기 저장(안에 조금 남음)

--- src/main/java/com/edume/createLecture/model/CreateLectureDAO.java
+++ src/main/java/com/edume/createLecture/model/CreateLectureDAO.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public interface CreateLectureDAO {
 
-
-	public int createLectureSave(CreateLectureDTO dto);
-	
+	public int createLectureInsert(); //저장 처음 눌렀을 때
+	public int createLectureUpdate(CreateLectureDTO dto); //저장 1회이상 눌렀을 때
+	public CreateLectureDTO createLectureContent(Map map); /*저장 후 안에 내용 저장해서 그대로 돌아가기*/
 }
