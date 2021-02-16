34fbd7312d81f69aa750109fb671ebd0c483a73a
seona <seona@DESKTOP-TFDSTUN>
2021-02-16 18:06:50 +0900

    강좌만들기 커리큘럼부분_선아

---
+++ src/main/java/com/edume/createLectureCurriculum/model/CreateLectureCurriculumDAOImple.java
@@ -0,0 +1,18 @@
+package com.edume.createLectureCurriculum.model;
+
+import java.util.*;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+public class CreateLectureCurriculumDAOImple implements CreateLectureCurriculumDAO {
+
+	@Autowired
+	private SqlSessionTemplate sqlMap;
+	
+
+
+
+}
